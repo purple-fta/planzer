0defdb0aa826f4e3f71c8aaeab431b0e453186b3
Baioretto <helloworldfirstfta@gmail.com>
2023-06-18 12:08:47 +0300

    Write tests
    - Type error
    - Value error

--- test/core_test/test_planzer_core_module.py
+++ test/core_test/test_planzer_core_module.py
@@ -1,4 +1,4 @@
-from lib.core import PlanzerCore
+from lib.core import *
 
 import pytest
 
@@ -12,30 +12,66 @@ def setup_function():
     planzer_core = PlanzerCore()
 
 
-@pytest.mark.parametrize(("filter"), ([1, 2, 3],
+@pytest.mark.parametrize("filter", ([1, 2, 3],
                                       "123", 123))
 def test_get_task_list_with_type_error(filter):
+    print(filter)
     with pytest.raises(TypeError):
         planzer_core.get_task_list(filter)
 
-def test_add_task_with_type_error():
-    pass
+@pytest.mark.parametrize(("task"), ([1, 2, 3],
+                                    "123", 123))
+def test_add_task_with_type_error(task):
+    with pytest.raises(TypeError):
+        planzer_core.add_task(task)
 
-def test_get_timeline_with_type_error():
-    pass
+@pytest.mark.parametrize(("day"), ([1, 2, 3],
+                                   "123", 123))
+def test_get_timeline_with_type_error(day):
+    with pytest.raises(TypeError):
+        planzer_core.get_timeline(day)
 
-def test_task_to_event_with_type_error():
-    pass
+@pytest.mark.parametrize(("task", "options"), ( [1123, "123"],
+                                                ["12", [1, 2, 3]],
+                                                ["12", [1, "1", 3]] ))
+def test_task_to_event_with_type_error(task, options):
+    with pytest.raises(TypeError):
+        planzer_core.task_to_event(task, options)
+
+@pytest.mark.parametrize(("filter"), (1, "123", 123))
+def test_create_TaskListDisplayOptions_with_type_error(filter):
+    with pytest.raises(TypeError):
+        TaskListDisplayOptions(filter)
 
 
-def test_get_task_list_with_value_error():
-    pass
+@pytest.mark.parametrize(("filter"), ([1, 2, 3],
+                                      "123", 123))
+def test_get_task_list_with_value_error(filter):
+    with pytest.raises(TypeError):
+        planzer_core.get_task_list(filter)
 
-def test_add_task_with_value_error():
-    pass
+@pytest.mark.parametrize(("task"), ([1, 2, 3],
+                                    "123", 123))
+def test_add_task_with_value_error(task):
+    with pytest.raises(TypeError):
+        planzer_core.add_task(task)
 
-def test_get_timeline_with_value_error():
-    pass
+@pytest.mark.parametrize(("day"), ([1, 2, 3],
+                                   "123", 123))
+def test_get_timeline_with_value_error(day):
+    with pytest.raises(TypeError):
+        planzer_core.get_timeline(day)
 
-def test_task_to_event_with_value_error():
-    pass
+@pytest.mark.parametrize(("task", "options"), ( [1123, "123"],
+                                                ["12", [1, 2, 3]],
+                                                ["12", [1, "1", 3]] ))
+def test_task_to_event_with_value_error(task, options):
+    with pytest.raises(TypeError):
+        planzer_core.task_to_event(task, options)
+
+@pytest.mark.parametrize(("tags"), ([1, Tag("tag", (0, 150, 150))],
+                                    (Tag("tag", (0, 150, 150)), Tag),
+                                    [1, Tag, 5]))
+def test_create_TaskListDisplayOptions_with_value_error(tags):
+    with pytest.raises(ValueError):
+        TaskListDisplayOptions(tags)
