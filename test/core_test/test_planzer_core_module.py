6996734201b40a90c3804212217c57136d54f83d
Baioretto <helloworldfirstfta@gmail.com>
2023-06-18 17:08:41 +0300

    Delate unnecessary tests
    - test_add_task_with_value_error. Все данные будут правильные.
    - test_get_timeline_with_value_error. Аналогично
    - и т.д.

--- test/core_test/test_planzer_core_module.py
+++ test/core_test/test_planzer_core_module.py
@@ -49,25 +49,6 @@ def test_get_task_list_with_value_error(filter):
     with pytest.raises(TypeError):
         planzer_core.get_task_list(filter)
 
-@pytest.mark.parametrize(("task"), ([1, 2, 3],
-                                    "123", 123))
-def test_add_task_with_value_error(task):
-    with pytest.raises(TypeError):
-        planzer_core.add_task(task)
-
-@pytest.mark.parametrize(("day"), ([1, 2, 3],
-                                   "123", 123))
-def test_get_timeline_with_value_error(day):
-    with pytest.raises(TypeError):
-        planzer_core.get_timeline(day)
-
-@pytest.mark.parametrize(("task", "options"), ( [1123, "123"],
-                                                ["12", [1, 2, 3]],
-                                                ["12", [1, "1", 3]] ))
-def test_task_to_event_with_value_error(task, options):
-    with pytest.raises(TypeError):
-        planzer_core.task_to_event(task, options)
-
 @pytest.mark.parametrize(("tags"), ([1, Tag("tag", (0, 150, 150))],
                                     (Tag("tag", (0, 150, 150)), Tag),
                                     [1, Tag, 5]))
