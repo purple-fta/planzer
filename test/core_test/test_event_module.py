1a17303d01d9b09d70caa4af439ebaec619ac8c8
Baioretto <helloworldfirstfta@gmail.com>
2023-07-06 17:31:33 +0300

    Logic test for event

--- test/core_test/test_event_module.py
+++ test/core_test/test_event_module.py
@@ -26,3 +26,8 @@ def test_create_EventOptions_with_value_error(options):
 def test_create_Event_with_value_error():
     with pytest.raises(ValueError):
         Event(Task("1", Priority.high, [], 0, datetime.datetime(2023, 10, 5)), EndDuration(datetime.datetime(2024, 11, 6), datetime.timedelta(10)))
+
+
+def test_get_event_duration():
+    event_options = EventOptions(StartEnd(datetime.datetime(2020, 5, 10), datetime.datetime(2021, 5, 11, 8, 21)))
+    assert event_options.get_event_duration() == datetime.timedelta(days=366, seconds=30060)
