1b63356cb2d37c32e69660a7382e0a4a982de355
Baioretto <helloworldfirstfta@gmail.com>
2023-07-12 13:16:41 +0300

    For last commit

--- lib/save_load_system/save_load_system.py
+++ lib/save_load_system/save_load_system.py
@@ -8,7 +8,7 @@ class SaveLoadSystem:
     Args:
         file_name (str): Имя файла для записи. УНИКАЛЕН
         to_dict_func (function): Функция преобразующая информацию из классов в словарь, 
-                                 который будет сохранён как JSON файл. Принимает *args
+                                 который будет сохранён как JSON файл. Принимает args
         from_dict_func (function): Функция принимающая словарь и возвращающая исходную 
                                    информацию.
     """
@@ -26,7 +26,7 @@ class SaveLoadSystem:
         Сохраняет словарь от to_dict_func с переданными args в JSON файл
 
         Args:
-            *args: Аргументы, что будут переданы в to_dict_func
+            args: Аргументы, что будут переданы в to_dict_func
 
         Raises:
             InvalidNumberOfArguments: если from_dict_func не принимает аргументы
