06c7eb6811a28826ba2f7d978bb6a0b08a1f6bf0
Євгеній Друзенко <druzenko.y.r@gmail.com>
2023-07-12 15:38:53 +0300

    save and load funk

--- lib/save_load_system/save_load_system.py
+++ lib/save_load_system/save_load_system.py
@@ -1,4 +1,8 @@
+import os.path
 from typing import Any
+import json
+
+
 
 
 class SaveLoadSystem:
@@ -12,16 +16,37 @@ class SaveLoadSystem:
         from_dict_func (function): Функция принимающая словарь и возвращающая исходную 
                                    информацию.
     """
+
+
+
     def __init__(self, file_name: str, to_dict_func: function, from_dict_func: function):
         pass
 
+   # def to_dict_func(self, *args):
+
+
     def load(self) -> Any:
+
+        # Визначаємо шлях до файлу JSON
+        json_file_path = "file.json"
+
+        # Відкриваємо файл у режимі читання
+        with open(json_file_path, "r") as file:
+            # Завантажуємо дані з файлу JSON
+            data = json.load(file)   #dict
+
+        return from_dict_func(data)
         """
         Возвращает результат из JSON переданный в from_dict_func
 
         """
 
     def save(self, args) -> None:
+        path=""  #TODO
+        file_name="" #TODO
+        dict_data=to_dict_funk(data)
+        with open(os.path.join(path,file_name),"w") as file:
+            json.dump(dict_data,file)
         """
         Сохраняет словарь от to_dict_func с переданными args в JSON файл
 
