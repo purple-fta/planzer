5dbfb6f5a3cf722e014997b9c3636b0ac79651e4
Baioretto <helloworldfirstfta@gmail.com>
2023-07-13 15:00:00 +0300

    Refactoring
    - Delate unused TODO
    - function to Callable
    - Incorrect argument for self.to_dict_func. Data is not defined

--- lib/save_load_system/save_load_system.py
+++ lib/save_load_system/save_load_system.py
@@ -1,5 +1,5 @@
 import os.path
-from typing import Any
+from typing import Any, Callable
 import json
 
 
@@ -17,48 +17,42 @@ class SaveLoadSystem:
                                    информацию.
     """
 
-    def __init__(self, file_name: str, to_dict_func: function, from_dict_func: function):
+    def __init__(self, file_name: str, to_dict_func: Callable, from_dict_func: Callable):
         self.file_name=file_name
         self.to_dict_func=to_dict_func
         self.from_dict_func=from_dict_func
 
-        pass  # TODO: Что бы использовать file_name, to_dict_func и from_dict_func нужно использовать self
-
 
     def load(self) -> Any:
         """
-             Возвращает результат из JSON переданный в from_dict_func
-
-             """
-        # TODO: """ Вот такой комментарий с """ должен быть в начале, а не в конце. 
-        #           Это описание функции и того, что в ней будет происходить """
+        Возвращает результат из JSON переданный в from_dict_func
+        """
 
         # Визначаємо шлях до файлу JSON
-        json_file_path = self.file_name  # TODO: нужно использовать file_name из конструктора класса
+        json_file_path = self.file_name
 
         # Відкриваємо файл у режимі читання
         with open(json_file_path, "r") as file:
             # Завантажуємо дані з файлу JSON
             data = json.load(file)   #dict
 
-        return self.from_dict_func(data) # TODO: from_dict_func нужно сохранить в self.from_dict_func
+        return self.from_dict_func(data)
 
 
     def save(self, args) -> None:
         """
-                Сохраняет словарь от to_dict_func с переданными args в JSON файл
+        Сохраняет словарь от to_dict_func с переданными args в JSON файл
 
-                Args:
-                    args: Аргументы, что будут переданы в to_dict_func
+        Args:
+            args: Аргументы, что будут переданы в to_dict_func
 
-                Raises:
-                    InvalidNumberOfArguments: если from_dict_func не принимает аргументы
-
-                """
-        # TODO: Тут в начале должен быть многострочный комментарий, который сейчас в конце
+        Raises:
+            InvalidNumberOfArguments: если from_dict_func не принимает аргументы
+        """
          
-        path=""  #TODO
-        file_name=self.file_name #TODO: нужно использовать file_name из self
-        dict_data=self.to_dict_func(data) # TODO: тоже из self нужно брать
-        with open(os.path.join(path,file_name),"w") as file:
-            json.dump(dict_data,file)
+        path = ""  #TODO
+        file_name = self.file_name 
+        dict_data = self.to_dict_func(args)
+
+        with open(os.path.join(path, file_name), "w") as file:
+            json.dump(dict_data, file)
