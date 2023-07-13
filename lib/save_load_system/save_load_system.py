74775b9f95189110f7a553d18fc5461897c491a3
Baioretto <helloworldfirstfta@gmail.com>
2023-07-13 11:51:28 +0300

    Added TODO

--- lib/save_load_system/save_load_system.py
+++ lib/save_load_system/save_load_system.py
@@ -17,34 +17,34 @@ class SaveLoadSystem:
                                    информацию.
     """
 
-
-
     def __init__(self, file_name: str, to_dict_func: function, from_dict_func: function):
-        pass
-
-   # def to_dict_func(self, *args):
+        pass  # TODO: Что бы использовать file_name, to_dict_func и from_dict_func нужно использовать self
 
 
     def load(self) -> Any:
+        # TODO: """ Вот такой комментарий с """ должен быть в начале, а не в конце. 
+        #           Это описание функции и того, что в ней будет происходить """
 
         # Визначаємо шлях до файлу JSON
-        json_file_path = "file.json"
+        json_file_path = "file.json"  # TODO: нужно использовать file_name из конструктора класса 
 
         # Відкриваємо файл у режимі читання
         with open(json_file_path, "r") as file:
             # Завантажуємо дані з файлу JSON
             data = json.load(file)   #dict
 
-        return from_dict_func(data)
+        return from_dict_func(data) # TODO: from_dict_func нужно сохранить в self.from_dict_func
         """
         Возвращает результат из JSON переданный в from_dict_func
 
         """
 
     def save(self, args) -> None:
+        # TODO: Тут в начале должен быть многострочный комментарий, который сейчас в конце
+         
         path=""  #TODO
-        file_name="" #TODO
-        dict_data=to_dict_funk(data)
+        file_name="" # TODO: нужно использовать file_name из self
+        dict_data=to_dict_funk(data) # TODO: тоже из self нужно брать
         with open(os.path.join(path,file_name),"w") as file:
             json.dump(dict_data,file)
         """
