abed71fef360a230dd79a2a8997fbffa674fd99c
Baioretto <helloworldfirstfta@gmail.com>
2023-08-02 19:13:49 +0300

    FEATURE: Plugin for tabs
    - Base struct
    - Added buttons
    - Without logic

---
+++ lib/PyQtGUI/standard_plugins/tabs_plugin.py
@@ -0,0 +1,26 @@
+from lib.PyQtGUI.AbstractPlugin import AbstractPlugin
+from lib.PyQtGUI.abstractMainWindow import AbstractMainWindow
+
+from PyQt5.QtWidgets import *
+
+
+class TabsPlugin(AbstractPlugin):
+    def __init__(self, ui: AbstractMainWindow) -> None:
+        super().__init__(ui)
+
+        self.first_tab_button = QToolButton()
+        self.second_tab_button = QToolButton()
+        
+        self.set_localization()
+        self.setup_ui()
+        
+    def set_localization(self):
+        self.first_tab_button.setText("1")
+        self.second_tab_button.setText("2")
+
+    def setup_ui(self):
+        self.ui.leftToolBar_addWidget(self.first_tab_button)
+        self.ui.leftToolBar_addWidget(self.second_tab_button)
+
+
+plugin_class = TabsPlugin
