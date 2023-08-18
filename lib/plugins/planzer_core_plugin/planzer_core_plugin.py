6faafa848d3b21539732e14d8b6b80480644a78a
Baioretto <helloworldfirstfta@gmail.com>
2023-08-18 21:19:48 +0300

    FEATURE: Create task in popup window

--- lib/plugins/planzer_core_plugin/planzer_core_plugin.py
+++ lib/plugins/planzer_core_plugin/planzer_core_plugin.py
@@ -1,7 +1,10 @@
-from PyQt5 import QtCore
+import datetime
+
+from PyQt5 import QtCore, QtGui
 from PyQt5.QtWidgets import QToolButton
 
 from lib.PyQtGUI.AbstractPlugin import *
+from lib.core import Tag, PlanzerCore
 from lib.plugins.planzer_core_plugin.plugin_widgets import *
 
 
@@ -12,11 +15,18 @@ class PlanzerCorePlugin(AbstractPlugin):
     def __init__(self, ui) -> None:
         super().__init__(ui)
 
+        self.core = PlanzerCore()
+
         self.create_task_button = QPushButton("+")
         self.list_tool_button  = QToolButton()
         self.calendar_tool_button = QToolButton()
 
-        self.popup_widget_create_task = None
+        self.task_list_window = TaskListWindow()
+
+        self.text_for_priority_combobox = {"High": Priority.high, "Normal": Priority.normal, "Low": Priority.low}
+        self.popup_widget_create_task = NewTaskPopupWidget(self.text_for_priority_combobox, self.ui)
+        self.popup_widget_create_task.hide()
+
 
         self.setup_localization()
         self.setup_ui()
@@ -42,12 +52,24 @@ class PlanzerCorePlugin(AbstractPlugin):
             Set connects to widgets
         """
         self.create_task_button.clicked.connect(self._show_create_task_window)
-        self.list_tool_button.clicked.connect(lambda: self.ui.add_widget_to_workspace(TaskListWindow()))
+        self.list_tool_button.clicked.connect(lambda: self.ui.add_widget_to_workspace(self.task_list_window))
         self.calendar_tool_button.clicked.connect(lambda: self.ui.add_widget_to_workspace(CalendarWindow()))
 
+        self.popup_widget_create_task.create_push_button.clicked.connect(lambda: self._create_task())
+
+    def _create_task(self):
+        name = self.popup_widget_create_task.name_input.text()
+        priority = self.text_for_priority_combobox[self.popup_widget_create_task.priority_combobox.currentText()]
+        tags = self.popup_widget_create_task.add_tags_widget.tags
+        task = Task(name, priority, tags, None, datetime.datetime.now())
+
+        self.core.add_task(task)
+        self.task_list_window.add_task(task)
+        self.task_list_window.high_priority_list.update()
+
+        self.popup_widget_create_task.reset_input_data()
+
     def _show_create_task_window(self):
-        if self.popup_widget_create_task is None:
-            self.popup_widget_create_task = NewTaskPopupWidget(self.ui)
         pos = self.create_task_button.mapTo(self.ui, QtCore.QPoint(0, 0))
         self.popup_widget_create_task.show(pos.x() + int(self.create_task_button.geometry().width() / 2), pos.y())
 
--- lib/plugins/planzer_core_plugin/plugin_widgets.py
+++ lib/plugins/planzer_core_plugin/plugin_widgets.py
@@ -1,8 +1,9 @@
+from PyQt5.QtGui import QColor
 from PyQt5.QtWidgets import QWidget, QSpacerItem, QSizePolicy, QCalendarWidget, QHBoxLayout, QLabel, QVBoxLayout, \
-                            QFrame, QLineEdit, QPushButton
+    QFrame, QLineEdit, QPushButton, QCheckBox, QComboBox
 
 from lib.PyQtGUI.KWidgets import KWorkspaceWindow, KCollapsibleBox
-from lib.core import Task, Priority
+from lib.core import Task, Priority, Tag
 
 
 class CalendarWindow(KWorkspaceWindow):
@@ -10,6 +11,14 @@ class CalendarWindow(KWorkspaceWindow):
         super().__init__("Calendar", parent)
 
         self.layout().addWidget(QCalendarWidget())
+
+
+class TagWidget(QLabel):
+    def __init__(self, tag: Tag, parent: QWidget | None = None):
+        super().__init__(f" #{tag.name} ", parent)
+        self.setStyleSheet(f"background-color: rgba{tag.decor.getRgb()}; border-radius: 5px")
+        self.setMaximumHeight(20)
+        self.setMinimumHeight(20)
 
 
 class TaskInList(QWidget):
@@ -26,9 +35,8 @@ class TaskInList(QWidget):
         tags_widget = QWidget()
         tags_widget.setLayout(QHBoxLayout())
         for tag in task.tags:
-            label = QLabel(tag.name)
-            label.setStyleSheet(f"background-color: rgba{tag.decor.getRgb()}; border-radius: 5px")
-            tags_widget.layout().addWidget(label)
+            tag_widget = TagWidget(tag)
+            tags_widget.layout().addWidget(tag_widget)
 
         tags_widget.layout().addItem(QSpacerItem(10, 20, QSizePolicy.Expanding, QSizePolicy.Minimum))
 
@@ -38,6 +46,7 @@ class TaskInList(QWidget):
         deadline_widget.layout().addWidget(QLabel(str(task.deadline)))
         deadline_widget.layout().addItem(QSpacerItem(10, 20, QSizePolicy.Expanding, QSizePolicy.Minimum))
 
+        self.layout().addWidget(QCheckBox())
         self.layout().addWidget(QLabel(task.name))
         self.layout().addWidget(tags_widget)
         self.layout().addWidget(deadline_widget)
@@ -74,31 +83,70 @@ class TaskListWindow(KWorkspaceWindow):
             self.low_priority_list.add_new_widget(task_widget)
 
 
+class TagInputWidget(QWidget):
+    def __init__(self, parent: QWidget | None = None):
+        super().__init__(parent)
+        self.tags = []
+
+        self.layout = QHBoxLayout()
+
+        self.input = QLineEdit()
+
+        self.layout.addWidget(self.input)
+
+        self.setLayout(self.layout)
+
+        self.input.returnPressed.connect(self.add_tag)
+
+    def add_tag(self):
+        new_tag = Tag(self.input.text(), QColor(189, 147, 249))
+        self.layout.insertWidget(self.layout.count()-1, TagWidget(new_tag))
+        self.input.setText("")
+        self.tags.append(new_tag)
+
+
 class NewTaskPopupWidget(QFrame):
     """
     Popup window for creating a task. Overrides all widgets
     """
-    def __init__(self, parent: QWidget | None = None) -> None:
+    def __init__(self, text_for_priority_combobox: dict[str, Priority], parent: QWidget | None = None) -> None:
         super().__init__(parent)
+        # TODO: выделить всё кроме инициализации полей в отдельные методы
+
+        self.name_input = QLineEdit()
+        self.priority_combobox = QComboBox()
+        self.add_tags_widget = TagInputWidget()
+        self.create_push_button = QPushButton("CREATE")
 
         self.setStyleSheet(
             "QFrame {background: rgba(68, 71, 90, 1); border-radius: 10px;}")  # Установите желаемый стиль виджета
-        self.setMinimumWidth(350)
+        self.setMinimumWidth(750)
         self.setMinimumHeight(100)
 
         QHBoxLayout(self)
 
         self.layout().addWidget(QLabel("POPUP"))
-        self.layout().addWidget(QLineEdit())
-        self.layout().addWidget(push_button := QPushButton("CREATE"))
+        self.layout().addWidget(self.name_input)
+        self.layout().addWidget(self.priority_combobox)
+        self.layout().addWidget(self.add_tags_widget)
+        self.layout().addWidget(self.create_push_button)
 
-        push_button.clicked.connect(self.push_button_clicked)
+        self.create_push_button.clicked.connect(self.push_button_clicked)
+        self.text_priority = text_for_priority_combobox
+        self.add_items_to_priority_combobox()
+
+    def add_items_to_priority_combobox(self):
+        for text in self.text_priority:
+            self.priority_combobox.addItem(text)
 
     def push_button_clicked(self):
         """
         Method called when the create button is clicked
         """
         self.hide()  # TODO: нужно как-то освободить память. Сейчас оно его скрывает, но не удаляет
+
+    def reset_input_data(self):
+        self.name_input.setText("")
 
     def show(self, x: int, y: int) -> None:
         """
@@ -112,4 +160,3 @@ class NewTaskPopupWidget(QFrame):
         # TODO: нужно ли переопределять метод?
         self.move(int(x - self.geometry().width() / 2), y + 30)
         super().show()
-
